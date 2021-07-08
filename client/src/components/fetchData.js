bd0d104c25fc9072fef397a180ede097a58a4256
basit-dev <bas677@icloud.com>
2021-07-09 00:40:07 +0100

    included api  error checking

--- client/src/components/fetchData.js
+++ client/src/components/fetchData.js
@@ -6,27 +6,51 @@ class FetchData extends React.Component {
   constructor() {
     super();
     this.state = {
+      error: null,
+      isLoaded: false,
       items: [],
     };
   }
   componentDidMount() {
     fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
       .then((res) => res.json())
-      .then((result) => {
-        this.setState({
-          items: result.data,
-        });
-      });
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.data,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
 
-  dataMap = () =>
-    this.state.items.map((data) => {
+  dataMap = () => {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
       return (
-        <div key={data._id}>
-          <q>{data.quoteText}</q>
-        </div>
+        <>
+          {items.map((data) => (
+            <div key={data._id}>
+              <q>{data.quoteText}</q>
+            </div>
+          ))}
+        </>
       );
-    });
+    }
+  };
 
   render() {
     return (
